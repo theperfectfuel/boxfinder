4949f933482218be02b40bc05afa4f36015f7f9d
theperfectfuel <robert@theperfectfuel.com>
2015-04-02 11:53:40 -0700

    Commit after working through initial template and bootstrap setup

--- boxfinder/settings.py
+++ boxfinder/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'boxfinder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(MAIN_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,6 +114,6 @@ TEMPLATE_DIRS = (
     os.path.join(MAIN_DIR, 'templates'),
     )
 
-STATIC_DIRS = (
+STATICFILES_DIRS = (
     os.path.join(MAIN_DIR, 'static'),
     )
